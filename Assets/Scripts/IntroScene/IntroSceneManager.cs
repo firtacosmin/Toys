d6b069b9ee000c549dfdf0813caf4e1a51aa2654
cosmin <cosmin.firta@visma.com>
2018-07-18 20:49:35 +0300

    start of high score manager implementation

--- Assets/Scripts/IntroScene/IntroSceneManager.cs
+++ Assets/Scripts/IntroScene/IntroSceneManager.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class IntroSceneManager : MonoBehaviour {
+    public Text highScoreText;
 
 	public void StartGame()
     {
         SceneManager.LoadScene("Scenes/Level 01");
+    }
+
+    private void Start()
+    {
+        if ( GameDataManager.instance.highScore >= 0 )
+        {
+            highScoreText.text = "HighScore: " + GameDataManager.instance.playerName + ":" + GameDataManager.instance.highScore;
+        }
+        else
+        {
+            highScoreText.text = "";
+        }
     }
 
     void Update()
---
+++ Assets/Scripts/Managers/GameDataManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class GameDataManager : MonoBehaviour {
+
+    private static String PLAYER_DATA_FILE = "playerData.dat";
+
+    public static GameDataManager instance;
+
+    public int highScore{
+        get { return playerData.highScore; }
+        set { playerData.highScore = value; }
+    }
+    public string playerName{
+        get { return playerData.PlayerName; }
+        set { playerData.PlayerName = value; }
+    }
+
+    private PlayerData playerData;
+
+	void Awake () {
+        if ( instance == null )
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+            init();
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    public void savePlayerData()
+    {
+        saveDataToStorage();    
+    }
+
+    private void init()
+    {
+        loadDataFromStorage();
+    }
+
+
+    private void loadDataFromStorage()
+    {
+        if ( File.Exists( Application.persistentDataPath + "/" + PLAYER_DATA_FILE ) )
+        {
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            FileStream fileStream = File.Open(Application.persistentDataPath + "/" + PLAYER_DATA_FILE, FileMode.Open);
+            playerData = (PlayerData)binaryFormatter.Deserialize(fileStream);
+            fileStream.Close();
+        }
+        else
+        {
+            playerData = new PlayerData();
+        }
+    }
+
+    private void saveDataToStorage()
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        FileStream fileStream = File.Open(
+            Application.persistentDataPath + "/" + PLAYER_DATA_FILE, 
+            FileMode.OpenOrCreate);
+        binaryFormatter.Serialize(fileStream, playerData);
+        fileStream.Close();
+    }
+
+}
+
+[Serializable]
+class PlayerData
+{
+    public int highScore;
+    public string PlayerName;
+}
